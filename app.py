3a4e0f7043cb5379ffd196b12d89737bfa8840b5
Chris Mague <github@mague.com>
2019-05-07 11:33:24 -0700

    load data if the key is not already present

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from redisearch import AutoCompleter, Suggestion
 import json
+import redis
 import csv
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return render_template('search.html')
-
-@app.route('/autocomplete')
-def auto_complete():
-    ac = AutoCompleter('ac')
-    name = request.args.get('term')
-    suggest = ac.get_suggestions(name, fuzzy = True)
-    return(json.dumps([{'value': item.string, 'label': item.string, 'id': item.string, 'score': item.score} for item in suggest]))
-
-@app.route('/loaddata')
 def load_data():
    ac = AutoCompleter('ac')
    with open('./fortune500.csv') as csv_file:
@@ -26,7 +15,21 @@ def load_data():
          if line_count > 0:
             ac.add_suggestions(Suggestion(row[1].replace('"', ''),  1.0))
          line_count += 1
-   return redirect("/", code=302)
+
+@app.route('/')
+def hello_world():
+   r = redis.Redis()
+   print(len(r.keys('ac')))
+   if len(r.keys('ac')) < 1:
+       load_data()
+   return render_template('search.html')
+
+@app.route('/autocomplete')
+def auto_complete():
+    ac = AutoCompleter('ac')
+    name = request.args.get('term')
+    suggest = ac.get_suggestions(name, fuzzy = True)
+    return(json.dumps([{'value': item.string, 'label': item.string, 'id': item.string, 'score': item.score} for item in suggest]))
 
 
 if __name__ == '__main__':
