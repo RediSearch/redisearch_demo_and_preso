70e32b331479928faa9958e102a789fadf2b8e1c
Chris Mague <github@mague.com>
2020-03-19 15:29:11 -0700

    get basic results display

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
-from redisearch import AutoCompleter, Suggestion
+from redisearch import AutoCompleter, Suggestion, Client, Query, TextField
+
 import json
 import redis
 import csv
@@ -17,23 +18,46 @@ topbar = Navbar('',
 )
 nav.register_element('top', topbar)
 
+def search_data(company):
+   client = Client('fortune500')
+   j = client.search(Query(company).limit_fields('title').verbatim())
+   return(j.docs[0].__dict__)
+
 def load_data():
    ac = AutoCompleter('ac')
+   client = Client('fortune500')
+   client.create_index([
+      TextField('title', weight=5.0),
+      TextField('website')
+   ])
    with open('./fortune500.csv') as csv_file:
       csv_reader = csv.reader(csv_file, delimiter=',')
       line_count = 0
       for row in csv_reader:
          if line_count > 0:
             ac.add_suggestions(Suggestion(row[1].replace('"', ''),  1.0))
+            client.add_document(
+               row[1].replace(" ", ''),
+               title = row[1],
+               website = row[2]
+               )
          line_count += 1
 
 @app.route('/')
 def index():
    r = redis.Redis()
-   print(len(r.keys('ac')))
    if len(r.keys('ac')) < 1:
        load_data()
    return render_template('search.html')
+
+@app.route('/display', methods = ['POST'])
+def display():
+   display = request.form
+   info = search_data(display['account'])
+   print(info)
+   return render_template('results.html', result = info)
+
+
 
 @app.route('/autocomplete')
 def auto_complete():
