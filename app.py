a6efb9c7e7a5daa5cc75974ddae5da771895a5ca
Chris Mague <github@mague.com>
2020-03-23 15:15:31 -0700

    added some example aggregations

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
-from redisearch import AutoCompleter, Suggestion, Client, Query, TextField
+from redisearch import AutoCompleter, Suggestion, Client, Query, TextField, NumericField, aggregation, reducers
 
 import json
 import redis
@@ -15,8 +15,14 @@ bootstrap = Bootstrap()
 nav = Nav()
 topbar = Navbar('',
     View('Home', 'index'),
+    View('Aggregations', 'show_agg'),
 )
 nav.register_element('top', topbar)
+
+def agg_by(field):
+   client = Client('fortune500')
+   ar = aggregation.AggregateRequest().group_by(field, reducers.count().alias('my_count')).sort_by(aggregation.Desc('@my_count'))
+   return (client.aggregate(ar).rows)
 
 def search_data(company):
    client = Client('fortune500')
@@ -31,12 +37,18 @@ def load_data():
    client.create_index([
       TextField('title', weight=5.0),
       TextField('website'),
-      TextField('employees'),
-      TextField('industry'),
-      TextField('hqlocatoin'),
+      NumericField('employees', sortable=True),
+      TextField('industry', sortable=True),
+      TextField('sector', sortable=True),
+      TextField('hqcity', sortable=True),
+      TextField('hqstate', sortable=True),
       TextField('ceo'),
       TextField('ceoTitle'),
-      TextField('rank'),
+      NumericField('rank', sortable=True),
+      NumericField('assets', sortable=True),
+      NumericField('revenues', sortable=True),
+      NumericField('profits', sortable=True),
+      NumericField('equity', sortable=True),
       TextField('ticker'),
    ])
    with open('./fortune500.csv') as csv_file:
@@ -51,11 +63,17 @@ def load_data():
                rank = row[0],
                website = row[2],
                employees = row[3],
-               industry = row[4],
-               hqlocation = row[5],
+               sector = row[4],
+               industry = row[5],
+               hqcity = row[8],
+               hqstate = row[9],
                ceo = row[12],
                ceoTitle = row[13],
-               ticker = row[15]
+               ticker = row[15],
+               revenues = row[17],
+               profits = row[19],
+               assets = row[21],
+               equity = row[22]
                )
          line_count += 1
 
@@ -72,7 +90,15 @@ def display():
    info = search_data(display['account'])
    return render_template('results.html', result = info)
 
+@app.route('/aggregate')
+def show_agg():
+   return render_template("aggregate.html")
 
+@app.route('/showagg', methods = ['POST'])
+def agg_show():
+   a = request.form.to_dict()
+   rows = agg_by(a['agg'])
+   return render_template('aggresults.html', rows = rows, field = a['agg'])
 
 @app.route('/autocomplete')
 def auto_complete():
